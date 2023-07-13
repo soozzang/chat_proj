cb4a2c454931185185827cd92304d409c47097c9
한수현 <soohyun@Soo-MacBookPro-2.local>
2023-07-14 00:56:18 +0900

    daphne작업 전

--- chat_proj/asgi.py
+++ chat_proj/asgi.py
@@ -1,5 +1,7 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_proj.settings')
 
@@ -7,13 +9,17 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from . import routing
 
-application = ProtocolTypeRouter(
-    {
-        "http" : get_asgi_application() ,
-        "websocket" : AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_urlpatterns
-            )   
-        )
-    }
-)
+django.setup()
+application = get_default_application()
+
+
+# application = ProtocolTypeRouter(
+#     {
+#         "http" : get_asgi_application() ,
+#         "websocket" : AuthMiddlewareStack(
+#             URLRouter(
+#                 routing.websocket_urlpatterns
+#             )   
+#         )
+#     }
+# )
